cec93823e661ffd9898cbbc3b499f2959491b637
Ondrej Holecek <ondrej.holecek@firma.seznam.cz>
2014-09-22 15:13:46 +0200

    #! elasticsearch fake

---
+++ elasticsearch.py
@@ -0,0 +1,14 @@
+
+class Indices:
+    def __init__(self):
+        pass
+    
+    def exists(self, key):
+        return True
+
+class Elasticsearch:
+    def __init__(self, p1):
+        self.indices = Indices()
+
+    def index(self, **params):
+        print "ES_INDEX: %s" % params
